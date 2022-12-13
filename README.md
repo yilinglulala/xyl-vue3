71c0551141b54e3eba3aee7f253f234bd2aba9cb
xuyil <xuyil@yealink.com>
2022-12-13 20:26:39 +0800

    fix: 删除无用代码

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+# todo
+- 引入 scss
+- 使用 axios
+- 配置表单
+  [ ] 下拉框子项如何配置
+  [ ] $t() 处理
+  [ ] v-bind
+- 配置表格
+  
+- 路由
+  -  menu 通过路由动态生成
+
 # tools
 
 This template should help get you started developing with Vue 3 in Vite.
--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <!--
  * @FilePath: \tools\src\App.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 入口
 -->
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
-
 <template>
-  <!-- <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/moment">Moment</RouterLink>
-        <RouterLink to="/json">JSON</RouterLink>
-      </nav>
-    </div>
-  </header> -->
-
   <RouterView />
 </template>
 
